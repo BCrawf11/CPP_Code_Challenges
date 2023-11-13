ed32369aab405198d665f1ff47396b43ab1f3be0
Brodie Crawford <44710785+BCrawf11@users.noreply.github.com>
2023-11-13 19:33:16 +0000

    Completed challenge 3

--- src/03/Challenge/ch3_FizzBuzz.cpp
+++ src/03/Challenge/ch3_FizzBuzz.cpp
@@ -6,16 +6,35 @@
 // The user enters the last number in the sequence.
 
 #include <iostream>
+#include <string>
 
 // FizzBuzz, main()
 // Summary: This application runs on the main function.
 int main(){
     int n; // How many numbers to include in the sequence.
-    
+    std::string message;
+
     std::cout << "Enter a positive integer: " << std::flush;
     std::cin >> n;
+    
+    bool m3, m5;
 
-    // Write your code here
+    for (int i = 1; i <= n; i++)
+    {
+        m3 = (i % 3 == 0);
+        m5 = (i % 5 == 0);
+        
+        if (!m3 && !m5)
+            std::cout << i;
+        else
+        {
+            if (m3)
+                std::cout << "Fizz";
+            if (m5)
+                std::cout << "Buzz";
+        }
+        std::cout << std::endl;
+    }
     
     std::cout << std::endl << std::flush;
     return 0;
