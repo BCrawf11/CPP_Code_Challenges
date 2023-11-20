ab3ec58e7731c94755634d46ceff4000f12c94cf
Brodie Crawford <44710785+BCrawf11@users.noreply.github.com>
2023-11-20 17:36:17 +0000

    Finished Challenge 6

--- src/06/Challenge/ch6_horseplay.cpp
+++ src/06/Challenge/ch6_horseplay.cpp
@@ -17,17 +17,43 @@
 // Returns: An STL vector of strings with the possible locations to move.
 std::vector<std::string> knight_moves(std::string knight){
     std::vector<std::string> moves;
+    int x, y;
 
-    std::vector<char> letters = "abcdefgh";
-    char kLetter = knight[0];
-    char kNumber = knight[1];
+    x = knight[0] - 'a' + 1;
+    y = knight[1] - '0';
 
-    for (int i = 0; i < 8; i++)
+    const int delta1[2] = { -1, 1};
+    const int delta2[2] = { -2, 2};
+
+    int new_x;
+    int new_y;
+    std::string new_loc = "  ";
+
+    for (int i = 0; i < 2; i++)
     {
-        int lIndex = std::find(letters, 8, kLetter);
-        if ()
-    }
+        for(int j = 0; j < 2; j++)
+        {
+            new_x = x + delta1[i];
+            new_y = y + delta2[j];
 
+            if(new_x > 0 && new_x < 9 && new_y > 0 && new_y < 9)
+            {
+                new_loc[0] = new_x + 'a' - 1;
+                new_loc[1] = new_y + '0';
+                moves.push_back(new_loc);
+            }
+
+            new_x = x + delta2[i];
+            new_y = y + delta1[j];
+
+            if(new_x > 0 && new_x < 9 && new_y > 0 && new_y < 9)
+            {
+                new_loc[0] = new_x + 'a' - 1;
+                new_loc[1] = new_y + '0';
+                moves.push_back(new_loc);
+            }
+        }
+    }
     return moves;
 }
 
